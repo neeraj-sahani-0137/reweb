60a069319f06170587222e3e09cf2ec153c4c84a
Niraj Kumar <neeraj.sahani0137@gmail.com>
2026-09-15 23:20:42 +0530

    Host and use fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import "./App.css";
 export default function App() {
   const [count, setCount] = createSignal(0);
   return (
-    <main class="flex-center size-dv flex-col gap-10 bg-slate-900 text-6xl text-mist-50 select-none">
+    <main class="flex-center size-dv flex-col gap-10 bg-slate-900 font-sans text-6xl text-mist-50 select-none">
       <h1>
-        Counter: <span class="font-jet">{count()}</span>
+        Counter: <span class="font-mono">{count()}</span>
       </h1>
       <button
         class="cursor-pointer rounded-md bg-rose-400 px-4 py-2 text-black"
